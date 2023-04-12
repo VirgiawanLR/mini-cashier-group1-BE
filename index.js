0a74cd764cd66fdb2b1bdff7e2c69af8abcec61c
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-04-12 19:49:40 +0700

    initialize backend
    first set up API, running on PORT: 8000

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express"); // initialize backend: set up express
+const cors = require("cors"); // initialize backend: cors for FE permission access
+const PORT = 8000; // initialize backend: defining PORT for the API
+const app = express(); // initialize backend: create the API
+
+app.use(express.json()); // initialize backend: json interpreter, so
+// your API understand JSON structure data
+app.use(cors());
+
+app.listen(PORT, () => {
+  // initialize backend: execute your API in defined PORT
+  console.log("SERVER RUNNING ON PORT", PORT);
+});
+
+// initialize backend: for authentication and authorization we need to install modules
+// making token: npm i jsonwebtoken express-bearer-token
+// npm install nodemailer
+// npm install bcrypt for hashing password
+// npm install mysql2 and util to make a connection into your databases
+// npm install multer to enable file uploading
