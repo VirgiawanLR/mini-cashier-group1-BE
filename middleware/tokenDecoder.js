7f4eb2b18799020abcf7e935d995cbd3e0ee3a68
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-04-13 20:31:43 +0700

    add feature
    keep login feature -> complete

--- middleware/tokenDecoder.js
+++ middleware/tokenDecoder.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   tokenDecoder: (req, res, next) => {
-    console.log("token decode", req.token);
     jwt.verify(req.token, "mini-cashier", (err, decode) => {
       if (err) {
         return res
@@ -10,7 +9,6 @@ module.exports = {
           .send({ message: "not a match user", isSuccess: false });
       }
       req.user = decode;
-      console.log("decoded", req.user);
       next();
     });
   },
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -6,5 +6,6 @@ const { tokenDecoder } = require("../middleware/tokenDecoder");
 routers.post("/register", usersController.registerUser);
 routers.patch("/verify-account", tokenDecoder, usersController.verifyAccount);
 routers.post("/login", usersController.loginAccount);
+routers.post("/keep-login", tokenDecoder, usersController.keepLogin);
 
 module.exports = routers;
