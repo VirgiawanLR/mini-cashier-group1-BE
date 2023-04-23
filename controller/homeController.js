6770db7eb86819981fb171fd7aedc7f06502f203
Priska Rafel <60578867+moonrafel@users.noreply.github.com>
2023-04-23 15:31:45 +0700

    Fixed Pagination system

--- controller/homeController.js
+++ controller/homeController.js
@@ -10,20 +10,4 @@ module.exports = {
       return res.status(error.statusCode || 500).send(error);
     }
   },
-  nextPage: async (req, res) => {
-    try {
-      const { indexStart, indexEnd } = req.body;
-      const products = await query(
-        `SELECT * FROM Products WHERE product_id BETWEEN ${db.escape(
-          indexStart
-        )} AND ${db.escape(indexEnd)}`
-      );
-      const count = products.length;
-      const productRange = { indexStart, indexEnd };
-      return res.status(200).send({ products, count, productRange });
-    } catch (error) {
-      return res.status(error.statusCode || 500).send(error);
-    }
-  },
-  createNewProduct: async (req, res) => {},
 };
--- router/homeRouter.js
+++ router/homeRouter.js
@@ -4,6 +4,5 @@ const { homeController } = require("../controller");
 const routers = express.Router();
 
 routers.get("/", homeController.getProductHome);
-routers.post("/next", homeController.nextPage);
 
 module.exports = routers;
