294b1f5d1f1c2dd08ccab8b2435acb4f192084f8
Priska Rafel <60578867+moonrafel@users.noreply.github.com>
2023-04-27 19:52:31 +0700

    Update productsController.js

--- controller/productsController.js
+++ controller/productsController.js
@@ -20,27 +20,23 @@ module.exports = {
       product_description,
       category_ID,
     } = req.body;
-    let createNewProductQuery = `INSERT into Users (product_name, product_price, product_quantity, product_image, product_description) values (${db.escape(
+
+    // console.log(req.body);
+
+    let createNewProductQuery = `INSERT into Products (product_name, product_price, product_quantity, product_image, product_description, category_ID) values (${db.escape(
       product_name
     )}, ${db.escape(product_price)}, ${db.escape(
       product_quantity
     )}, ${db.escape(product_image)}, ${db.escape(
       product_description
-    )}, ${db.escape(category_ID)})})`;
+    )}, ${db.escape(category_ID)})`;
+
+    console.log(createNewProductQuery);
+
     try {
       const product = await query(createNewProductQuery);
-      let payload = {
-        product_ID: product.insertId,
-        product_price,
-        product_quantity,
-        product_image,
-        product_description,
-        category_ID,
-      };
-
-      return res
-        .status(200)
-        .send({ message: `${product_name} has been created!` });
+      console.log(product);
+      return res.status(200).send({ message: `Product has been created!` });
     } catch (error) {
       return res.status(error.statusCode || 500).send(error);
     }
